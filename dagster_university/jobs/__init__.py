91c57165faba50c1ab414781731e636aad07b9d7
Cathaff <catharinafrindt@msn.com>
2024-06-20 18:00:30 +0200

    test sensor in dagster

--- dagster_university/jobs/__init__.py
+++ dagster_university/jobs/__init__.py
@@ -2,17 +2,23 @@ from dagster import AssetSelection, define_asset_job
 from ..partitions import monthly_partitions, weekly_partitions
 
 trips_by_week = AssetSelection.assets("trips_by_week")
+adhoc_request = AssetSelection.assets("adhoc_request")
 
 trip_update_job = define_asset_job(
     name="trip_update_job",
     partitions_def=monthly_partitions,
-    selection=AssetSelection.all() - trips_by_week
+    selection=AssetSelection.all() - trips_by_week - adhoc_request
 )
 
 weekly_update_job = define_asset_job(
     name="weekly_update_job",
     partitions_def=weekly_partitions,
     selection=trips_by_week
+)
+
+adhoc_request_job = define_asset_job(
+    name="adhoc_request_job",
+    selection=adhoc_request
 )
 
 
